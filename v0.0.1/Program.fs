df1222acddc786672f80f0f61631636eb260d4d3
Masataka Wachi <m-wachi@debian01>
2022-06-29 07:56:07 +0900

    add code.

--- v0.0.1/Program.fs
+++ v0.0.1/Program.fs
@@ -3,38 +3,17 @@
 open System.IO
 open FSharp.Text.Lexing
 
-open Ast
-
-let testLexerAndParserFromString text expectedCount = 
-    let lexbuf = LexBuffer<char>.FromString text
-
-    let countFromParser = Parser.start Lexer.tokenstream lexbuf
-
-(*
-    printfn "countFromParser: result = %d, expected %d" countFromParser expectedCount
-*)
-    printfn "%s" (Ast.progToStr countFromParser)
-    
-    
-let testLexerAndParserFromFile (fileName:string) expectedCount = 
+let parseFile (fileName: string) =
     use textReader = new System.IO.StreamReader(fileName)
     let lexbuf = LexBuffer<char>.FromTextReader textReader
 
-    let countFromParser = Parser.start Lexer.tokenstream lexbuf
-(*
-    printfn "countFromParser: result = %d, expected %d" countFromParser expectedCount
-*)
-    printfn "%s" (Ast.progToStr countFromParser)
+    Parser.start Lexer.tokenstream lexbuf
     
 
-testLexerAndParserFromString "hello" 1
-testLexerAndParserFromString "hello hello" 2
-
 let testFile = Path.Combine(__SOURCE_DIRECTORY__, "test.txt")
-File.WriteAllText(testFile, "hello hello")
-testLexerAndParserFromFile testFile 2
+let prsRes = parseFile testFile
 
-printfn "Press any key to continue..."
-System.Console.ReadLine() |> ignore
+printfn "%s" (Ast.progToStr prsRes)
+
 
 
